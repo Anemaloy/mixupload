97a360a94799ce741721a44aae62c72e5699d1f7
a.rybin <Anemaliy@mail.ru>
2020-03-18 17:34:13 +0300

    Parser v1.0

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # mixupload
+Parser for music mixupload.com
---
+++ index — копия.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+<body>
+<h2>Топ 100</h2>
+	<ul>
+		<li><a href="house100.php">house</a></li>
+		<li><a href="bass100.php">Bass house</a></li>
+		<li><a href="deep100.php">Deep house</a></li>
+		<li><a href="chicago100.php">Chicago house</a></li>
+		<li><a href="club100.php">Club house</a></li>
+		<li><a href="electro100.php">Electro house</a></li>
+		<li><a href="funky100.php">Funky house</a></li>
+		<li><a href="ghouse100.php">G house</a></li>
+		<li><a href="latino100.php">latino house</a></li>
+		<li><a href="moombathon100.php">Moombathon house</a></li>
+		<li><a href="tropical100.php">Tropical house</a></li>
+		<li><a href="hhp100.php">Hip hop</a></li>
+	</ul>
+	<h2>Последние 200</h2>
+	<ul>
+		<li><a href="house100.php">Bass house</a></li>
+		<li><a href="bass100.php">Bass house</a></li>
+		<li><a href="deep100.php">Deep house</a></li>
+		<li><a href="chicago100.php">Chicago house</a></li>
+		<li><a href="club100.php">Club house</a></li>
+		<li><a href="electro100.php">Electro house</a></li>
+		<li><a href="funky100.php">Funky house</a></li>
+		<li><a href="ghouse100.php">G house</a></li>
+		<li><a href="latino100.php">latino house</a></li>
+		<li><a href="moombathon100.php">Moombathon house</a></li>
+		<li><a href="tropical100.php">Tropical house</a></li>
+		<li><a href="hhp100.php">Hip hop</a></li>
+	</ul>
+</body>
+</html>
